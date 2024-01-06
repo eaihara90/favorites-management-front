90181235925b967573a507088e692a8244a2db17
Eduardo Aihara <eaihara90@gmail.com>
2024-01-05 21:14:36 -0300

    feat: button props

--- src/ui/atoms/icon-button/icon-button.tsx
+++ src/ui/atoms/icon-button/icon-button.tsx
@@ -1,15 +1,16 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 import './icon-button.scss';
 
-interface IconButtonProps {
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void; 
 }
 
-export function IconButton({ children, onClick }: IconButtonProps): JSX.Element {
+export function IconButton({ children, onClick, ...props }: IconButtonProps): JSX.Element {
   return (
     <button
+      {...props}
       className="icon-button"
       onClick={onClick}>
       {children}
